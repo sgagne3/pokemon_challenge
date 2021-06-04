ef8218f7ad209942e7a696e941ac50b13b49c68c
sgagne3 <sgagne3@coveo.com>
2021-06-04 09:39:52 -0400

    path changes

--- bin/js/coveo.extension.js
+++ bin/js/coveo.extension.js
@@ -127,13 +127,13 @@ module.exports = Coveo;
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.hu = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return "b705826-" + chunkId + "-wps-hmr.js";
+/******/ 			return "41e2437-" + chunkId + "-wps-hmr.js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/get update manifest filename */
 /******/ 	(() => {
-/******/ 		__webpack_require__.hmrF = () => ("b705826-wps-hmr.json");
+/******/ 		__webpack_require__.hmrF = () => ("41e2437-wps-hmr.json");
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
